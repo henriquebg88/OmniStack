33393140c332c46ae90c12130a130f3637e6e680
Henrique <henriquebg88@gmail.com>
2020-04-06 19:11:22 -0300

    Validation on the backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { celebrate, Joi, Segments } = require('celebrate');
 const OngController = require('./controllers/OngController.js');
 const IncidentController = require('./controllers/IncidentControllers.js');
 const ProfileController = require('./controllers/ProfileControllers.js');
@@ -6,19 +7,45 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+var ongBodyData = {
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email:Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+};
+var profileHeaderData = {
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+};
+var incidentParamsData = {
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+};
+var pageNumberData = {
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+};
+
+
 routes.get('/', (req, resp) => {
     resp.send("Home page");
 })
 
 routes.post('/session', SessionController.create);
 
-routes.get('/profiles', ProfileController.index);
+routes.get('/profiles', celebrate(profileHeaderData), ProfileController.index);
 
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate(pageNumberData) ,IncidentController.index);
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/incidents/:id',celebrate(incidentParamsData) ,IncidentController.delete);
 
 routes.get('/ongs', OngController.index);
-routes.post("/ongs", OngController.create);
+routes.post("/ongs", celebrate(ongBodyData) , OngController.create);
 
 module.exports = routes;
